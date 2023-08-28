cb38c450b76ac1ccf01d931e766d03b7e5c19a09
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-08-28 12:02:36 +0300

    git domain model of the entire project(Mendix SDK)

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mendixplatformsdk_1 = require("mendixplatformsdk");
+async function main() {
+    const client = new mendixplatformsdk_1.MendixPlatformClient();
+    const app = await client.getApp("fa517ca0-7b0c-4db6-9b64-039cfed357e7");
+    const workingCopy = await app.createTemporaryWorkingCopy("main");
+    const model = await workingCopy.openModel();
+    const fs = require("fs");
+    const modulesCreatedByDevelopers = model.allModules().filter((module) => module.fromAppStore === false);
+    fs.writeFileSync("modulesCreatedByDevelopers.json", JSON.stringify(modulesCreatedByDevelopers, null, 2));
+    let entitiesAndAssociations = [];
+    const getProjectEntitiesAndAssociations = async (modules, index) => {
+        if (modules[index] === undefined || modules[index] === null) {
+            fs.writeFileSync("EntitiesAndAssociations.json", JSON.stringify(entitiesAndAssociations, null, 2));
+            return entitiesAndAssociations;
+        }
+        const getModule = modules[index];
+        const loadModule = await getModule.domainModel.load();
+        const entities = loadModule.entities.map((entity) => entity.toJSON());
+        const associations = loadModule.associations.map((association) => association.toJSON());
+        const crossAssociations = loadModule.crossAssociations.map((crossAssociation) => crossAssociation.toJSON());
+        //combine all entites together then all associations together then all cross associations together then push to array
+        entitiesAndAssociations = [
+            ...entitiesAndAssociations,
+            { entities: entities, associations: associations, crossAssociations: crossAssociations },
+        ];
+        getProjectEntitiesAndAssociations(modules, index + 1);
+    };
+    getProjectEntitiesAndAssociations(modulesCreatedByDevelopers, 0);
+    await model.flushChanges();
+    await workingCopy.commitToRepository("main");
+}
+main().catch(console.error);
---
+++ script.ts
@@ -0,0 +1,44 @@
+import { domainmodels } from "mendixmodelsdk";
+import { MendixPlatformClient } from "mendixplatformsdk";
+import { isModuleReference } from "typescript";
+
+async function main() {
+  const client = new MendixPlatformClient();
+  const app = await client.getApp("fa517ca0-7b0c-4db6-9b64-039cfed357e7");
+
+  const workingCopy = await app.createTemporaryWorkingCopy("main");
+  const model = await workingCopy.openModel();
+
+  const fs = require("fs");
+
+  const modulesCreatedByDevelopers = model.allModules().filter((module) => module.fromAppStore === false);
+  fs.writeFileSync("modulesCreatedByDevelopers.json", JSON.stringify(modulesCreatedByDevelopers, null, 2));
+
+  let entitiesAndAssociations = [] as any;
+
+  const getProjectEntitiesAndAssociations = async (modules: any, index: any) => {
+    if (modules[index] === undefined || modules[index] === null) {
+      fs.writeFileSync("EntitiesAndAssociations.json", JSON.stringify(entitiesAndAssociations, null, 2));
+      return entitiesAndAssociations;
+    }
+    const getModule = modules[index];
+    const loadModule = await getModule.domainModel.load();
+    const entities = loadModule.entities.map((entity: any) => entity.toJSON());
+    const associations = loadModule.associations.map((association: any) => association.toJSON());
+    const crossAssociations = loadModule.crossAssociations.map((crossAssociation: any) =>
+      crossAssociation.toJSON()
+    );
+    //combine all entites together then all associations together then all cross associations together then push to array
+    entitiesAndAssociations = [
+      ...entitiesAndAssociations,
+      { entities: entities, associations: associations, crossAssociations: crossAssociations },
+    ];
+    getProjectEntitiesAndAssociations(modules, index + 1);
+  };
+  getProjectEntitiesAndAssociations(modulesCreatedByDevelopers, 0);
+  await model.flushChanges();
+
+  await workingCopy.commitToRepository("main");
+}
+
+main().catch(console.error);
